758780b3e5dc06dc1dbc983a0f02dba6a9bdf36e
Davis Inde Satya <davis.onde.wei@gmail.com>
2024-04-22 08:56:33 +0700

    added phaser scene

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import Phaser from "phaser";
+
+
+window.onload = () => {
+    const game = Phaser.Game()
+}
---
+++ scripts/scene.js
@@ -0,0 +1,11 @@
+import Phaser from "phaser";
+
+class Scene extends Phaser.Scene {
+    preload() {
+        this.load.image('background', '../resource/underwater.gif');
+    }
+
+    create() {
+        
+    }
+}
