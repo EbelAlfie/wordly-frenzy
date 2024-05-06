ef974cd01ca1436231caf64bf370e2f47e919cc8
EbelAlfie <davis.onde.wei@gmail.com>
2024-05-07 01:00:00 +0700

    food?

--- scripts/food.js
+++ scripts/food.js
@@ -11,7 +11,7 @@ export class Food extends Phaser.Physics.Arcade.Sprite {
 
         this.setScale(config.scale);
 
-        this.setCollideWorldBounds(false);
+        this.setCollideWorldBounds(true);
 
         this.score = config.value ;
 
